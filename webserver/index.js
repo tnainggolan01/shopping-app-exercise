9587ed9cff460c65a285ef6b7efa49b188a21fe9
cokubear <cokubear@gmail.com>
2024-10-03 18:21:11 -0700

    Making setup for deployment on a hosting service

--- webserver/index.js
+++ webserver/index.js
@@ -24,6 +24,14 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
+// Use the client-side public folder for static files
+app.use(express.static(__dirname + "/webclient/build"));
+
+// Render the React app for any path
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/webclient/build/index.html");
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
